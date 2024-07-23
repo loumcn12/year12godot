5fd086e21f843716d2885bf55b481daefb2af5a0
loumcn12 <loumcn12@kingshigh.school.nz>
2024-07-24 11:23:23 +1200

    Fixed door

--- NCEA Game 2024/Scripts/door.gd
+++ NCEA Game 2024/Scripts/door.gd
@@ -6,13 +6,6 @@ var door_open = false
 func _ready():
 	open_door()
 	open_door()
-#
-#func _physics_process(delta):
-	#if door_open and door_moving:
-		#rotation.y = move_toward(rotation.y, (rotation.y + deg_to_rad(120)), 0.05)
-		#
-	#elif !door_open and door_moving:
-		#rotation.y = move_toward(rotation.y, (rotation.y - deg_to_rad(120)), 0.05)
 
 func open_door():
 	
